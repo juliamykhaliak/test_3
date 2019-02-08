59381c2ab434b2c2bf82f2709ce2bcf972238562
Julia Mykhaliak <yulya.mhl@gmail.com>
2019-02-08 17:16:56 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+//Add class for active mask.
+$('#feedback-link').on('click', function () {
+  $('.modal-window').addClass('active-mask');
+  $('.popup').addClass('active-popup');
+});
+
+//Remove class of active mask, click on modal window.
+$('.modal-window').on('click', function () {
+  $('.modal-window').removeClass('active-mask');
+  $('.popup').removeClass('active-popup');
+});
+
+//Remove class of active mask when click on close image.
+$('#close-popup-block').on('click', function () {
+  $('.modal-window').removeClass('active-mask');
+  $('.popup').removeClass('active-popup');
+});
+
+//Add class to body to hide overflow.
+$('#feedback-link').on('click', function () {
+  $('body').addClass('overflow');
+});
+$('.modal-window').on('click', function () {
+  $('body').removeClass('overflow');
+});
+
+//Slider
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items:1,
+    autoplayTimeout:4000,
+    navClass: ['left', 'right'],
+    autoplay:true,
+    loop:true,
+    animateOut: 'fadeOut',
+    navContainerClass: 'wrap-slider',
+  });
+});
+
+
+
